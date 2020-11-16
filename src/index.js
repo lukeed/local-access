539bccd510cd4eb797b4f13eb0c48dfbba0500b3
Luke Edwards <luke.edwards05@gmail.com>
2020-11-16 12:48:25 -0800

    feat: add `module` entry

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { format } = require('url');
-const { networkInterfaces } = require('os');
+import { format } from 'url';
+import { networkInterfaces } from 'os';
 
 const hostname = 'localhost';
 const port = process.env.PORT || 8080;
@@ -8,7 +8,7 @@ function isLAN(obj) {
 	return obj.family === 'IPv4' && !obj.internal;
 }
 
-module.exports = function (opts) {
+export default function (opts) {
 	opts = Object.assign({ hostname, port, https:false }, opts);
 	opts.protocol = opts.https ? 'https' : 'http';
 
@@ -16,7 +16,7 @@ module.exports = function (opts) {
 	let local = format(opts);
 	let nets = networkInterfaces();
 	for (k in nets) {
-		if (tmp=nets[k].find(isLAN)) {
+		if (tmp = nets[k].find(isLAN)) {
 			opts.hostname = tmp.address; // network IP
 			break;
 		}
